a97e161d175f76e44a6d1c71f2a93cde7111a3af
alemonacoo <ale.monaco@outlook.com>
2022-12-14 16:10:19 +0100

    fixing server.js

--- src/server.js
+++ src/server.js
@@ -1,27 +1,22 @@
 import express from "express";
-
-//cors: serve per utilizzare risorse web da qualunque punto del web
 import cors from "cors";
 
-// tutte le routes specifiche vanno importate
 import myRouter from "./services/index.js";
 import listEndpoints from "express-list-endpoints";
 
 const server = express();
-
-//prende i dati direttamente dal file .env
 const port = process.env.PORT;
 
 server.use(cors());
 server.use(express.json());
 
-// usa il router dal file importato con "prefisso" /cities
-server.use("/cities", myRouter);
+server.listen(port, async () => {
+  console.log("server is running correctly on port: ", port);
+});
 
-//mostra tutti gli endpoint come table (uguale ad un console log ma più ordinato)
+server.use("/", myRouter);
+
 console.table(listEndpoints(server));
-
-server.listen(port, () => console.log("server is running on port ", port));
-server.on("error", (error) =>
-  console.log("server not running due to: ", error)
-);
+server.on("error", (error) => {
+  console.log("server connection failed due to: ", error);
+});
